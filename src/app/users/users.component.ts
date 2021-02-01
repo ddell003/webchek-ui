2443ecf383fb756951ffc4c4ba207cd2201c56b6
Parker Dell <parker.dell@maxar.com>
2021-02-01 13:05:53 -0500

    added files

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user.model';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  /**
+   * Notes
+   * make a user service
+   * tie to the user service
+   * make api call to get user
+   *
+   */
+  constructor() { }
+
+  createUser = false;
+  users: User[];
+
+  ngOnInit(): void {
+  }
+
+  edit(user: User){
+
+  }
+  deleteUser(user: User) {
+
+  }
+
+}
