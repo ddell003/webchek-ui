d2b4cd7a94fe53b27d465245aed892e9ed44e5b0
Parker Dell <parker.dell@maxar.com>
2021-02-13 12:31:16 -0500

    fixed tests

--- src/app/entrance/entrance.component.spec.ts
+++ src/app/entrance/entrance.component.spec.ts
@@ -1,5 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { EntranceComponent } from './entrance.component';
 
@@ -11,6 +13,8 @@ describe('EntranceComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ EntranceComponent ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [HttpClientModule, RouterTestingModule],
+      providers: [HttpClientModule, HttpClientModule],
     })
     .compileComponents();
   });
--- src/app/sites/tests/edit/edit.component.spec.ts
+++ src/app/sites/tests/edit/edit.component.spec.ts
@@ -1,20 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
-import { EditComponent } from './edit.component';
+import { EditTestComponent } from './edit.component';
 
 describe('EditComponent', () => {
-  let component: EditComponent;
-  let fixture: ComponentFixture<EditComponent>;
+  let component: EditTestComponent;
+  let fixture: ComponentFixture<EditTestComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EditComponent ]
+      declarations: [ EditTestComponent ],
+      imports: [HttpClientModule, RouterTestingModule],
+      providers: [HttpClientModule, HttpClientModule],
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(EditComponent);
+    fixture = TestBed.createComponent(EditTestComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
